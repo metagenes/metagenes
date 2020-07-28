7a605f9b3145533dd16dd858b501dfdad5af4d51
Yudi Prayoga Raharja <yudiprayogaraharja@gmail.com>
2020-07-28 15:30:39 +0700

    update fun facts

--- scripts/rb/update_readme.rb
+++ scripts/rb/update_readme.rb
@@ -24,6 +24,7 @@ end
 markdown = <<~EOF
 ### Hi there 👋
 I like learning new things because I believe that as a software engineer you should keep learning new things and while also have some goal of what to achieve. After all, stacks that being used are always have something new to offer.
+
 Fun facts, while I'm not crying doing coding I love listening to a podcast and baking or just messing with a recipe that  I found online
 - 😄 Pronouns: He/Him
 - 🌱 I’m currently learning DevOps

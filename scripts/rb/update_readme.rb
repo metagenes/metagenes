255bd0fe1a406f71487b44d4ea8cf11b1b821841
Yudi Prayoga Raharja <yudiprayogaraharja@gmail.com>
2020-07-28 15:16:08 +0700

    update route

--- scripts/rb/update_readme.rb
+++ scripts/rb/update_readme.rb
@@ -2,14 +2,14 @@
 require "json"
 require "faraday"
 
-# Get alupdated posts
+# Get all posts
+# Take a look how we obtain our secret key by using ENV[]
 response = Faraday.get(
-  "api.github.com/users/metagenes/repos?sort=updated&per_page=5"
+  "https://api.github.com/users/metagenes/repos?sort=updated&per_page=5"
 )
 
 # Retrieve `title`, `url`, and `description` and
 # wrap it to markdown syntax
-
 posts = JSON.parse(response.body).map do |article|
   <<~EOF
   
@@ -18,26 +18,17 @@ posts = JSON.parse(response.body).map do |article|
   EOF
 end
 
-
 # Generate your own layout and paste posts in it
 # Don't forget to change text and name =)
 markdown = <<~EOF
 ### Hi there 👋
-
 I like learning new things because I believe that as a software engineer you should keep learning new things and while also have some goal of what to achieve. After all, stacks that being used are always have something new to offer.
-
 Fun facts, while I'm not crying doing coding I love listening to a podcast and baking or just messing with a recipe that  I found online
-
-
-
 - 😄 Pronouns: He/Him
 - 🌱 I’m currently learning DevOps
-
 ![counter](https://ene3oosohyebu4a.m.pipedream.net)
-
 My last publications:
 #{posts.join}
-
 EOF
 
 # Write you markdown to README.MD
